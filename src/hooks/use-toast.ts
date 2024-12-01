922018c9ac3c6a1888820b0c22bae62f59bb107f
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-12-01 19:30:59 +0530

    Refactor use-toast.ts to remove unused state variable and improve code structure

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -17,8 +17,6 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-type ActionType = 'ADD_TOAST' | 'UPDATE_TOAST' | 'DISMISS_TOAST' | 'REMOVE_TOAST'
-
 let count = 0
 
 function genId() {
